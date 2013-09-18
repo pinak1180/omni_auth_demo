d2b7bbb79d0e1a18df16e9a0452714bdbb11f994
Pinak <pinak@complitech.net>
2013-09-18 12:48:05 +0530

    changes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,9 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
+  require 'omniauth-twitter'
+  config.omniauth :twitter ,"V7ydOqdBYKKNCA6kctmng", "tY7XdZcgMobJDY564egvWXAVd31Doaz3kvpg8jlb4"
+
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
